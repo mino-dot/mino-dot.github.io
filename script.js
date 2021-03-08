31ab34c140262336e732f9bace50f1b1a3771205
Mino <adm1nmin0@gmail.com>
2021-03-08 17:49:26 -0500

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,10 @@ function myFunction() {
 function dud() {
     console.log("Hello");
 }
+
+var myVar = setInterval(myTimer, 1000);
+
+function myTimer() {
+  var d = new Date();
+  console.log(d.toLocaleTimeString());
+}
