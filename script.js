ee34c0c39fb74f1f951cab7c6ca8168b5f321788
Mino <adm1nmin0@gmail.com>
2021-03-08 17:30:16 -0500

    Funny Thing

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ function myFunction() {
     document.getElementsByClassName("btn btn-dark");
     alert('Hello');
 }
+
+function test() {
+    console.log("Hello, if you are reading this, DON'T")
+}
